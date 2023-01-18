185bd3dd647b0659eafb0aeb7f0db18c6863b005
JimmyL26 <99210847+JimmyL26@users.noreply.github.com>
2023-01-18 13:57:55 -0500

    Add files via upload

--- ex.js
+++ ex.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 app.get('/', (req,res) => {
     res.send('Hello there');
 });
@@ -33,3 +34,38 @@ app.listen(3000, () => {
 // localhost:3000/api/courses/2 - {"id":2,"name":"IT"}
 // localhost:3000/api/courses/3 - {"id":3,"name":"Cybersecurity"}
 // localhost:3000/api/courses/4 - The course with the given ID was not found
+
+app.post('/api/courses', (req,res) => {
+    // you write the if code here
+    //add an if statement so that the name of the course you post is .min(3) characters 
+    if (req.body.name.length > 3) {
+        const course ={
+            //we assign an ID and a name property
+            id: courses.length +1,
+            name:req.body.name
+        };
+        //YOU WRITE THE NEXT LINES OF code
+        //next step: push it to the array
+        //next step: the server should return the new resource to the client in the body of the response 
+        courses.push(course);
+        res.send(course);
+    }
+    else {
+        res.status(404).send("A name is required and should be greater than 3 characters long");
+    }  
+});
+
+app.put('/api/courses/:id', (req,res)=>{
+
+});
+
+app.delete('/api/courses/:id', (req,res)=>{
+    //code the following logic
+    //look up the course by id
+        //return 404 if does not exist
+        //delete the course by index HINT: use the indexOf() and splice() methods
+        // return the response to the client the course that was deleted
+});
+
+    
+    
