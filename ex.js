35532c0ff20b79d1c3c01bf60d313fb4826a5b3c
JimmyL26 <99210847+JimmyL26@users.noreply.github.com>
2023-01-18 20:52:52 -0500

    Update ex.js

--- ex.js
+++ ex.js
@@ -56,7 +56,26 @@ app.post('/api/courses', (req,res) => {
 });
 
 app.put('/api/courses/:id', (req,res)=>{
-
+    //Write the code in order to look up the course, if not existing return a 404
+    n = true;
+    courses.forEach(element => {
+        if(req.body.name === element.name){
+            n = false;
+            res.status(404).send("Course already exists")
+        }
+    });
+    if (n){
+        const course ={
+            //we assign an ID and a name property
+            id: req.body.id,
+            name: req.body.name
+        };
+        courses.splice(req.body.id - 1, 1, course);
+        res.send(course);
+    }
+            //otherwise 
+                    //update the course
+                    //return the updated course
 });
 
 app.delete('/api/courses/:id', (req,res)=>{
@@ -65,6 +84,18 @@ app.delete('/api/courses/:id', (req,res)=>{
         //return 404 if does not exist
         //delete the course by index HINT: use the indexOf() and splice() methods
         // return the response to the client the course that was deleted
+    if (courses.forEach(element => {
+        if(element.id === req.body.id){
+            return false;
+        }
+    })){
+        res.status(404).send("Course doesn't exist")
+    } else {
+        const delcourse = courses.slice(req.body.id -1, req.body.id);
+        courses.splice(req.body.id - 1, 1);
+        res.send(delcourse);
+    }
+
 });
 
     
